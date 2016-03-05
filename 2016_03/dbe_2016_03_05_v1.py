9a230996e49072baefe18d4f40ac3e3645027104
eliheuer <eliheuer@gmail.com>
2016-03-05 13:23:35 -0500

    wip

--- 2016_03/dbe_2016_03_05_v1.py
+++ 2016_03/dbe_2016_03_05_v1.py
@@ -17,53 +17,71 @@ def new_page():
     frameDuration(1/24) # set the dividend to desired FPS (frames per second) 
     fill(0.8) #light grey
     rect(0, 0, canvas, canvas) # background
-    
-# draws the red dot from to position variables    
-def dot_one(dot_one_x, dot_one_y):
-    fill(1, 0, 0) # red
-    stroke(None) # removes default outline
-    oval(int(dot_one_x) + center, int(dot_one_y) + center, 
-    circle_size, circle_size)
-    
-# draws a grid from a given increment 
-def grid(increment):    
+        
+def grid(origin, width, height, 
+         num_horizontal_divisions, num_vertical_divisions):
     fill(None)
-    stroke(0.5)
-    strokeWidth(1) 
-   
-    # draw the grid X-axis
-    step_x  = -increment               
-    for x in range(17):
-        step_x = step_x + increment
-        polygon((margin + step_x, margin), 
-        (margin+step_x, canvas-margin))
+    stroke(0.5) # color
+    strokeWidth(1)
     
-    # draw the grid Y-axis
-    step_y  = -increment  
-    for y in range(17):
-        step_y = step_y + increment
-        polygon((margin, margin + step_y), 
-        (canvas-margin, margin+step_y))
+    translate(*origin)
+    step_x = 0 
+    increment_x = width / num_horizontal_divisions
+    for x in range(num_horizontal_divisions + 1):
+        line((step_x, 0), (step_x, height))
+        step_x += increment_x
+        
+    step_y = 0 
+    increment_y = height / num_vertical_divisions
+    for y in range(num_vertical_divisions + 1):
+        line((0, step_y), (width, step_y))
+        step_y += increment_y
+                     
+        
+
+# # draws a grid from a given increment 
+# def grid(increment, canvas, margin):    
+#     fill(None)
+#     stroke(0.5)
+#     strokeWidth(1) 
+   
+#     # draw the grid X-axis
+#     step_x  = -increment               
+#     for x in range(17):
+#         step_x = step_x + increment
+#         line((margin + step_x, margin), 
+#             (margin+step_x, canvas-margin))
+    
+#     # draw the grid Y-axis
+#     step_y  = -increment  
+#     for y in range(17):
+#         step_y = step_y + increment
+#         line((margin, margin + step_y), 
+#             (canvas-margin, margin+step_y))
         
 # setting variables
 canvas = 512  # size of the gif in pixels
 margin = 128  # grid distance from edge of canvas 
 increment = 16  # grid increment
-num_frames = 90  # number of frames in the animation
+num_frames = 32  # number of frames in the animation
 circle_size = 12  # self explanatory
 center = int(canvas / 2) - 6 # exact center of the image
 amp = 128 # short for amplitude
 step = 0 # step in the animation
-color_cycle = ([1, 0, 0],[0, 0, 0],[0, 0, 0])
+origin = (32, 64)
+width = 256
+height = 256
+num_horizontal_divisions = 16
+num_vertical_divisions = 32
 
 # draw each frame as a new page
 for frame in range(num_frames):
-    new_page() 
-    grid(increment) 
-    dot_one_x = math.cos(step) * amp
-    dot_one_y = -1 * math.sin(step) * amp
-    dot_one(dot_one_x, dot_one_y)
-    step += 0.62831 # pi * 2 
-    step %= 8 * math.pi
+    new_page()
+    grid(origin, width, height, 
+         num_horizontal_divisions, num_vertical_divisions)
+    num_horizontal_divisions += 1
+    width +=8
+    
+    
     
 saveImage("dbe_2016_03_05_v1.gif")
