b2f6fd8311c9d0a705ae814480ec461508451d69
eliheuer <eliheuer@gmail.com>
2016-03-14 00:53:53 -0400

    sine loop: done

--- 2016_03/dbe_2016_03_12_v1.py
+++ 2016_03/dbe_2016_03_12_v1.py
@@ -11,11 +11,10 @@
 
 # from drawBot import * # uncomment if using setupAsModule.py
 import math # for cos and sin functions
-import random
 
 # static variables
 canvas = 512  # size of the gif in pixels
-num_frames = 32  # number of frames in the animation
+num_frames = 64  # number of frames in the animation
 
 # gird variables
 origin = (128, 128)
@@ -31,19 +30,19 @@ amp = 80 # short for amplitude
 step = 0 # step in the animation
 x_pos = 0 # x-axis position
 y_pos = 0 # y-axis position
-r = 1
-g = 0
-b = 0
-r_array = range(0, 256)
-g_array = range(0, 256)
-b_array = range(0, 256)
+r = 0.2
+g = 0.2
+b = 0.2
+r_array = range(-1600, 10000, 16)
+g_array = range(-1600, 10000, 16)
+b_array = range(-1600, 10000, 16)
 print "r_array: ", r_array
 color_cycle = 0
 
 # draws a new frame in the animation
 def new_page(): 
     newPage(canvas, canvas) # new page from canvas variable
-    frameDuration(1/24) # set the dividend to desired FPS (frames per second) 
+    frameDuration(1/20) # set the dividend to desired FPS (frames per second) 
     fill(0.1) # color of background
     rect(0, 0, canvas, canvas) # draw the background
 
@@ -77,17 +76,17 @@ for frame in range(num_frames):
     new_page()
     grid(origin, width, height, num_x_units, num_y_units)
     
-    # animated  dot
-    for sub_frame in range(num_frames*2):
-        r = r_array[sub_frame] * 0.0039
-        g = g_array[sub_frame] * 0.0039
-        b = b_array[sub_frame] * 0.0039
+    # animated dot
+    for sub_frame in range(num_frames*8):
+        r = r_array[sub_frame] * 0.0001
+        g = g_array[sub_frame] * 0.0001
+        b = g_array[sub_frame] * 0.0001
         color_cycle += 1
-        x_pos = math.cos(step/2) * amp
+        x_pos = math.cos(step/2) * amp*2
         y_pos = -1 * math.sin(step) * amp/2
 
         red_dot((x_pos)-dot_size/2, (y_pos)-dot_size/2, r, g, b)
-        step += 0.4
+        step += 0.1001
         step %= 8 * math.pi
            
 saveImage("dbe_2016_03_12_v1.gif")
