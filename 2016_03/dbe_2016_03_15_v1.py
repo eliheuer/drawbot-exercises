a3e791244754459d76f84b71a61cec58373a8453
eliheuer <eliheuer@gmail.com>
2016-03-15 14:40:34 -0400

    sine loop: working

--- 2016_03/dbe_2016_03_15_v1.py
+++ 2016_03/dbe_2016_03_15_v1.py
@@ -24,17 +24,14 @@ center = width/2
 num_x_units = 8
 num_y_units = 8
 
-# dot variables
-dot_size = 16  
-amp = 128 # short for amplitude
-step = 0 # step in the animation
-fake_step = math.pi # a dumb hack >_<
-step_string = "{:.8f}".format(fake_step)
-x_pos = 0 # x-axis position
-y_pos = 0 # y-axis position
-r = 1
-g = 1
-b = 1
+# circle variables
+circle_w = 256  #
+circle_h = 256  #
+circle_x = 0    #
+circle_y = 0    #
+divisions = 8   
+step = 0
+amp = 0 
 
 # draws a new frame in the animation
 def new_page(): 
@@ -50,8 +47,7 @@ def dot(x_pos, y_pos, r, b, g):
     oval(int(x_pos) + center, int(y_pos) + center, dot_size, dot_size)
     
 # draws a guide showing the sine of dot    
-def sine_lines(width, height, x_pos, y_pos, r, b, g):
-    fill(r, g, b)
+def draw_circle_old_news(width, height, x_pos, y_pos, divisions):
     stroke(None)
     oval(center-dot_size/2, center-dot_size/2, dot_size, dot_size)
     oval(int(x_pos) + center, center-dot_size/2, dot_size, dot_size)
@@ -65,32 +61,37 @@ def sine_lines(width, height, x_pos, y_pos, r, b, g):
     line((int(x_pos) + (center+dot_size/2), center), 
         (int(x_pos) + (center+dot_size/2), int(y_pos) + (center+dot_size/2)))
     oval(center-128, center-128, width, height)
-       
-# draws a grid from given arguments    
-def grid(origin, width, height, num_x_units, num_y_units):
-    translate(*origin)
-    strokeWidth(1)
-    stroke(0.5) 
-    fill(None)
     
-    step_x = 0 
-    unit_x = width / num_x_units
-    for x in range(num_x_units + 1):
-        line((step_x, 0), (step_x, height))
-        step_x += unit_x
+# draws a guide showing the sine of dot    
+def draw_circle(circle_w, circle_h, circle_x, circle_y, divisions):
+    
+    for segment in range(divisions):
+        # create a new empty path
+        newPath()
+        stroke(3)
+        fill(1, 0, 0)
+        # set the first oncurve point
+        moveTo((128, 128))
+        # line to from the previous point to a new point
+        lineTo((100, 200))
+        lineTo((200, 200))
+        # curve to a point with two given handles
+        lineTo((128,128))
+        # close the path
+        closePath()
+        # draw the path
+        drawPath()
         
-    step_y = 0 
-    unit_y = height / num_y_units
-    for y in range(num_y_units + 1):
-        line((0, step_y), (width, step_y))
-        step_y += unit_y
+def draw_type():
+    fontSize(24)
+    font("Helvetica Neue Bold")
+    tracking(0)
+    fill(1, 1, 1)
+    stroke(None)
+    text("Happy Hardcore", (-2, -32))
 
-# draw each frame as a new page
-for frame in range(num_frames):
-    new_page()
-    grid(origin, width, height, num_x_units, num_y_units)
-    
-    # animated dot
+def anime():
+        # animated dot
     x_pos = math.cos(step) * amp
     y_pos = -1 * math.sin(step) * amp
     x_pos_string = "{:.3f}".format(x_pos)
@@ -98,21 +99,34 @@ for frame in range(num_frames):
     print "x position: ", x_pos_string
     print "y position: ", y_pos_string
     
-    sine_lines(width, height, (x_pos)-dot_size/2, (y_pos)-dot_size/2, r, g, b)
-    dot((x_pos)-dot_size/2, (y_pos)-dot_size/2, r, g, b)
+# draws a grid from 5 arguments, see 'gird variables' above
+def draw_grid(origin, width, height, num_x_units, num_y_units):
+    unit_y = (height / num_y_units)
+    unit_x = (width / num_x_units)
+    translate(*origin)
+    strokeWidth(1)
+    stroke(0.5) 
+    fill(None)
+    step_x = 0 
+    step_y = 0 
     
-    # type 
-    fontSize(24)
-    font("Helvetica Neue Bold")
-    tracking(0)
-    fill(1, 1, 1)
-    stroke(None)
-    text("Happy π Day", (-2, -32))
-    fill(1, 0, 0)
-    text(step_string, (-2, -64))
+    for x in range(num_x_units + 1):
+        line((step_x, 0), (step_x, height))
+        step_x += unit_x
+        
+    for y in range(num_y_units + 1):
+        line((0, step_y), (width, step_y))
+        step_y += unit_y
+                
+# draw each frame as a new page
+for frame in range(num_frames):
+    new_page()
+    draw_grid(origin, width, height, num_x_units, num_y_units)
+
+    draw_circle(circle_w, circle_h, circle_x, circle_y, divisions)
     
+    draw_type()
+
     step += 0.02 * math.pi
-    step_string = "{:.8f}".format(fake_step)
-    fake_step -= math.pi / 100
                
 saveImage("dbe_2016_03_15_v2.gif")
