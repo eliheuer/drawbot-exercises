6275f38f5a7ff17546e91b81fc5283c867953345
eliheuer <eliheuer@gmail.com>
2016-03-18 16:42:31 -0400

    working

--- 2016_03/dbe_2016_03_17_v1.py
+++ 2016_03/dbe_2016_03_17_v1.py
@@ -33,9 +33,10 @@ divisions = 0
 # box variables
 box_size_x = 36 
 box_size_y = 36
-box_amp = 126
+box_amp = 110
 box_step = 0
-box_count = 25
+box_count = 1
+box_rt = 0
 
 # draws a new frame in the animation
 def new_page(): 
@@ -45,22 +46,22 @@ def new_page():
     rect(0, 0, canvas, canvas) 
 
 # draws the dot from two position variables    
-def box(x_pos, y_pos, box_size_x, box_size_y):
+def box(x_pos, y_pos, box_size_x, box_size_y, box_rt):
     fill(1)
     strokeWidth(1)
     stroke(0.2)
-    rect(int(x_pos) + center, int(y_pos) + center, 
-        box_size_x, box_size_y)
+    rotate(box_rt)
+    rect((x_pos - 2) + center, (y_pos - 2) + center, box_size_x, box_size_y)
     
 # draws a circle from 5 arguments, see 'gird variables' above
-def draw_path(path_x, path_y, box_count, dot_amp, box_step):
-   
+def draw_path(path_x, path_y, box_count, dot_amp, box_step, box_rt):
+     
     for segment in range(box_count):
-        box_x = math.cos(box_step) * (box_amp - 16)
-        box_y = -1 * math.sin(box_step) * box_amp
-        box((box_x)-box_size_x/2, (box_y)-box_size_y/2, box_size_x, box_size_y)
+        box_x = math.cos(box_step) * box_amp
+        box_y = math.sin(box_step) * box_amp
+        box(box_x, box_y, box_size_x, box_size_y, box_rt)
         
-        box_step += 0.02 * math.pi
+        box_step += 0.2 * math.pi
        
 # draws a grid from given arguments    
 def grid(origin, width, height, num_x_units, num_y_units):
@@ -82,13 +83,15 @@ def grid(origin, width, height, num_x_units, num_y_units):
         step_y += unit_y
 
 # draw each frame as a new page
+
 for frame in range(num_frames):
     new_page()
     grid(origin, width, height, num_x_units, num_y_units)
-        
-    draw_path(path_x, path_y, box_count, box_amp, box_step)
+    translate(-16, -16)
+    draw_path(path_x, path_y, box_count, box_amp, box_step, box_rt)
     box_count_string = "{:03d}".format(box_count)
-    #box_count += 1
-    box_step += 0.2
+    #box_count += 4
+    box_step += 0.1
+    box_rt += 0.01
     
 saveImage("dbe_2016_03_17_v1.gif")
