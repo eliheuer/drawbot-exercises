d5fab39cf93c0956a4070b9876739d849e3f7cee
eliheuer <eliheuer@gmail.com>
2016-03-19 00:56:19 -0400

    working

--- 2016_03/dbe_2016_03_18_v1.py
+++ 2016_03/dbe_2016_03_18_v1.py
@@ -22,8 +22,8 @@ origin = (128, 128)
 width = 256
 height = 256
 center = width/2
-num_x_units = 7
-num_y_units = 7
+num_x_units = 8
+num_y_units = 8
 
 # path variables
 path_x = 0
@@ -31,12 +31,12 @@ path_y = 0
 divisions = 0
 
 # box variables
-box_size_x = 36 
-box_size_y = 36
-box_amp = 74
+box_size_x = 64
+box_size_y = 40
+box_amp = 34
 box_step = 0
-box_count = 51
-box_rt = 0
+box_count = 24
+box_rt = 2
 
 def new_page(): 
     newPage(canvas, canvas) 
@@ -48,7 +48,7 @@ def box(x_pos, y_pos, box_size_x, box_size_y, box_rt):
     fill(1)
     strokeWidth(1)
     stroke(0.3)
-    rotate(box_rt)
+    rotate(3)
     rect((x_pos - 2) + center, (y_pos - 2) + center, box_size_x, box_size_y)
     
 def draw_path(path_x, path_y, box_count, dot_amp, box_step, box_rt):
@@ -56,9 +56,11 @@ def draw_path(path_x, path_y, box_count, dot_amp, box_step, box_rt):
     for segment in range(box_count):
         box_x = math.cos(box_step) * box_amp
         box_y = math.sin(box_step) * box_amp
+        box_rt += 0.4
         box(box_x, box_y, box_size_x, box_size_y, box_rt)
+
         
-        box_step += 0.01 * math.pi
+        box_step += 0.01 #* math.pi
        
 def grid(origin, width, height, num_x_units, num_y_units):
     translate(*origin)
@@ -81,7 +83,8 @@ def grid(origin, width, height, num_x_units, num_y_units):
 for frame in range(num_frames):
     new_page()
     grid(origin, width, height, num_x_units, num_y_units)
-    translate(-16, -16)
+    translate(128, -64)
+    rotate(30)
     draw_path(path_x, path_y, box_count, box_amp, box_step, box_rt)
     box_count_string = "{:03d}".format(box_count)
     box_step += 0.1
