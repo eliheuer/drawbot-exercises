70b19e58a072063cda8d654a78c05de7234747c7
eliheuer <eliheuer@gmail.com>
2016-03-20 02:29:38 -0400

    looping half rings: done

--- 2016_03/dbe_2016_03_19_v1.py
+++ 2016_03/dbe_2016_03_19_v1.py
@@ -31,36 +31,33 @@ path_y = 0
 divisions = 0
 
 # box variables
-dot_size_x = 16 
-dot_size_y = 16
-dot_amp = 128 - 16
-dot_step = 25
-dot_count = 51
+dot_size_x = 20
+dot_size_y = 20
+dot_amp = 120
+dot_step = 0
+dot_count = 100
 
 def new_page(): 
     newPage(canvas, canvas) 
     frameDuration(1/24) 
-    fill(0.1) 
+    fill(0.025, 0.025, 0.1) 
     rect(0, 0, canvas, canvas) 
         
-def dot(dot_x, dot_y, dot_size_x, dot_size_y):
-    fill(1, 0, 0)
+def dot(dot_x, dot_y, dot_size_x, dot_size_y, fill):
     stroke(None)
     oval((dot_x - 2) + center, (dot_y - 2) + center, dot_size_x, dot_size_y)
     
-def draw_path(path_x, path_y, dot_count, dot_amp, dot_step):
+def draw_path(path_x, path_y, dot_count, dot_amp, dot_step, fill):
     for segment in range(dot_count):
-        dot_amp = math.cos(dot_step) + dot_amp
         dot_x = math.cos(dot_step) * dot_amp
         dot_y = math.sin(dot_step) * dot_amp
-        dot(dot_x-8, dot_y-8, dot_size_x, dot_size_y)
-        
+        dot(dot_x-8, dot_y-8, dot_size_x, dot_size_y, fill)     
         dot_step += 0.01 * math.pi
        
 def grid(origin, width, height, num_x_units, num_y_units):
     translate(*origin)
     strokeWidth(1)
-    stroke(1) 
+    stroke(0.025, 0.025, 0.1) 
     fill(None)
     
     step_x = 0 
@@ -78,16 +75,19 @@ def grid(origin, width, height, num_x_units, num_y_units):
 for frame in range(num_frames):
     new_page()
     grid(origin, width, height, num_x_units, num_y_units)
-    
-    draw_path(path_x, path_y, dot_count, dot_amp, dot_step)
-    draw_path(path_x, path_y, dot_count, dot_amp-24, dot_step-24)
-    draw_path(path_x, path_y, dot_count, dot_amp-48, dot_step-48)
-    draw_path(path_x, path_y, dot_count, dot_amp-72, dot_step-72)
-    
-    fill(1, 0, 0)
+    fill(0.9, 0, 0.1)
+    draw_path(path_x, path_y, dot_count, dot_amp, dot_step, fill)
+    fill(0.9, 0.25, 0.1)
+    draw_path(path_x, path_y, dot_count, dot_amp-24, dot_step-24, fill)
+    fill(0.9, 0.4, 0.1)
+    draw_path(path_x, path_y, dot_count, dot_amp-48, dot_step-48, fill)
+    fill(0.9, 0.6, 0.1)
+    draw_path(path_x, path_y, dot_count, dot_amp-72, dot_step-72, fill)
+    fill(0.9, 0.8, 0.1)
+    draw_path(path_x, path_y, dot_count, dot_amp-96, dot_step-96, fill)
+    fill(0.9, 1, 0.5)
     stroke(None)
-    oval(center-8, center-8, dot_size_x, dot_size_y)
-    
+    oval(center-10, center-10, dot_size_x, dot_size_y)
     dot_step += 0.1
     
 saveImage("dbe_2016_03_19_v1.gif")
