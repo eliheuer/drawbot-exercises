d83d6dc27d6a3b6edd6585daf8cc28b93120e998
eliheuer <eliheuer@gmail.com>
2016-03-23 12:35:09 -0400

    type animation: done

--- 2016_03/dbe_2016_03_22_v1.py
+++ 2016_03/dbe_2016_03_22_v1.py
@@ -10,18 +10,19 @@
 #/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#/#
 
 # from drawBot import * # uncomment if using setupAsModule.py
-import math 
+import math
+import itertools  
 
 # static variables
 canvas = 512 
-num_frames = 36
+num_frames = 58
 
 # gird variables
 origin = (32, 32)
-width = 448
+width = 448 - 128
 height = 448
 center = width/2
-num_x_units = 14
+num_x_units = 10
 num_y_units = 14
 
 #type
@@ -32,15 +33,22 @@ hw_rt = 0
 hw_fs = 36 * 2
 hw_x_pos = 0
 hw_y_pos = 0
+hw_t = 0 
+#itertools
+type_rt_list = [0, 0, 0, 0, 0, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0, 0, 0, 0, 0, 0]
+print len(type_rt_list)
+
+type_rt_step = itertools.cycle(type_rt_list)
 
 def new_page(): 
     newPage(canvas, canvas) 
-    frameDuration(1/4) 
+    frameDuration(1/24) 
     fill(0.8) 
     rect(0, 0, canvas, canvas) 
 
-def hello_world(hw_x_pos, hw_y_pos, hw_rt, hw_fs):
+def hello_world(hw_x_pos, hw_y_pos, hw_rt, hw_fs, hw_t):
     fill(0)
+    hw_rt = type_rt_step.next()
     fontSize(hw_fs)
     rotate(hw_rt)
     tracking(-2)
@@ -48,7 +56,7 @@ def hello_world(hw_x_pos, hw_y_pos, hw_rt, hw_fs):
     text("Hello World", (-4, 0))
        
 def grid(origin, width, height, num_x_units, num_y_units):
-    translate(*origin)
+    translate(96, 32)
     strokeWidth(1)
     stroke(0.5) 
     fill(None)
@@ -68,9 +76,14 @@ def grid(origin, width, height, num_x_units, num_y_units):
 for frame in range(num_frames):
     new_page()
     grid(origin, width, height, num_x_units, num_y_units)
-    translate(0, 0)
-    hello_world(hw_x_pos, hw_y_pos, hw_rt, hw_fs)
-    type_step += 1
-    hw_rt += 10  
+    fill(1, 0, 0)
+    stroke(None)
+    #oval(32, 352, 32, 32)
+    translate(-2, 0)
+    hello_world(hw_x_pos, hw_y_pos, hw_rt, hw_fs, hw_t)
+    hw_t += 4
+ 
+    
+    
 
 saveImage("dbe_2016_03_22_v1.gif")
