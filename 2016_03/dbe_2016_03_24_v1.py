6feba517f6b2f795bc9b30b4f08ce2a060f0c1b4
eliheuer <eliheuer@gmail.com>
2016-03-25 15:55:46 -0400

    working

--- 2016_03/dbe_2016_03_24_v1.py
+++ 2016_03/dbe_2016_03_24_v1.py
@@ -65,7 +65,7 @@ def draw_path(path_x, path_y, dot_count, dot_amp, dot_step, fill):
 def grid(origin, width, height, num_x_units, num_y_units):
     translate(*origin)
     strokeWidth(1)
-    stroke(0.025, 0.025, 0.1)  
+    stroke(0.1, 0.2, 0.2)  
     fill(None)
     
     step_x = 0 
@@ -83,21 +83,21 @@ def grid(origin, width, height, num_x_units, num_y_units):
 for frame in range(num_frames):
     new_page()
     grid(origin, width, height, num_x_units, num_y_units)
-    fill(0.9, 0, 0.1)
+    fill(0.9, 0.2, 0.1)
     draw_path(path_x, path_y, dot_count, dot_amp, dot_step+dot_shift*6, fill)
-    fill(0.9, 0.25, 0.1)
-    draw_path(path_x, path_y, dot_count, dot_amp-24, dot_step+dot_shift*5, fill)
     fill(0.9, 0.4, 0.1)
+    draw_path(path_x, path_y, dot_count, dot_amp-24, dot_step+dot_shift*5, fill)
+    fill(0.9, 0.9, 0.1)
     draw_path(path_x, path_y, dot_count, dot_amp-48, dot_step+dot_shift*4, fill)
-    fill(0.9, 0.6, 0.1)
+    fill(0.2, 0.8, 0.1)
     draw_path(path_x, path_y, dot_count, dot_amp-72, dot_step+dot_shift*3, fill)
-    fill(0.9, 0.8, 0.1)
+    fill(0.1, 0.6, 0.9)
     draw_path(path_x, path_y, dot_count, dot_amp-96, dot_step+dot_shift*2, fill)
-    fill(0.9, 0.9, 0.3)
+    fill(0.1, 0.3, 0.9)
     stroke(None)
     oval(center-10, center-10, dot_size_x, dot_size_y)
     dot_step += 0.1
-    dot_shift += 0.1
+    dot_shift += 0.01
     dot_count = seq_step.next()
     dot_count_string = "{:.1f}".format(dot_count)
         
