9f478cc03ac61e5ba557507cc49412668a2b3943
eliheuer <eliheuer@gmail.com>
2016-03-25 21:23:47 -0400

    rainbow loop: done

--- 2016_03/dbe_2016_03_24_v1.py
+++ 2016_03/dbe_2016_03_24_v1.py
@@ -25,12 +25,7 @@ center = width/2
 num_x_units = 8
 num_y_units = 8
 
-# path variables
-path_x = 0
-path_y = 0
-divisions = 0
-
-# box variables
+# dot variables
 dot_size_x = 20
 dot_size_y = 20
 dot_amp = 120
@@ -48,14 +43,14 @@ seq_step = itertools.cycle(seq)
 def new_page(): 
     newPage(canvas, canvas) 
     frameDuration(1/24) 
-    fill(0.025, 0.025, 0.1) 
+    fill(0.1, 0.1, 0.2) 
     rect(0, 0, canvas, canvas) 
         
 def dot(dot_x, dot_y, dot_size_x, dot_size_y, fill):
     stroke(None)
     oval((dot_x - 2) + center, (dot_y - 2) + center, dot_size_x, dot_size_y)
     
-def draw_path(path_x, path_y, dot_count, dot_amp, dot_step, fill, frame):
+def draw_path(dot_count, dot_amp, dot_step):
     for segment in range(dot_count):
         dot_x = math.cos(dot_step) * dot_amp
         dot_y = math.sin(dot_step) * dot_amp
@@ -65,7 +60,7 @@ def draw_path(path_x, path_y, dot_count, dot_amp, dot_step, fill, frame):
 def grid(origin, width, height, num_x_units, num_y_units):
     translate(*origin)
     strokeWidth(1)
-    stroke(0.1, 0.2, 0.2)  
+    stroke(0.5, 0.5, 0.7)  
     fill(None)
     
     step_x = 0 
@@ -83,21 +78,27 @@ def grid(origin, width, height, num_x_units, num_y_units):
 for frame in range(num_frames):
     new_page()
     grid(origin, width, height, num_x_units, num_y_units)
+    
     fill(0.9, 0.2, 0.1)
-    draw_path(path_x, path_y, dot_count, dot_amp, dot_step+dot_shift, fill, frame)
+    draw_path(dot_count, dot_amp, dot_step+dot_shift)
+    
     fill(0.9, 0.4, 0.1)
-    draw_path(path_x, path_y, dot_count, dot_amp-24, dot_step+dot_shift+0.2, fill, frame)
+    draw_path(dot_count, dot_amp-24, dot_step+dot_shift+0.2)
+    
     fill(0.9, 0.9, 0.1)
-    draw_path(path_x, path_y, dot_count, dot_amp-48, dot_step+dot_shift+0.4, fill, frame)
+    draw_path(dot_count, dot_amp-48, dot_step+dot_shift+0.4)
+    
     fill(0.2, 0.8, 0.1)
-    draw_path(path_x, path_y, dot_count, dot_amp-72, dot_step+dot_shift+0.6, fill, frame)
+    draw_path(dot_count, dot_amp-72, dot_step+dot_shift+0.6)
+    
     fill(0.1, 0.6, 0.9)
-    draw_path(path_x, path_y, dot_count, dot_amp-96, dot_step+dot_shift+0.8, fill, frame)
+    draw_path(dot_count, dot_amp-96, dot_step+dot_shift+0.8)
+    
     fill(0.1, 0.3, 0.9)
     oval(center-10, center-10, dot_size_x, dot_size_y)
+    
     dot_step += 0.095
     dot_shift += 0.01
     dot_count = seq_step.next()
-    dot_count_string = "{:.1f}".format(dot_count)
         
 saveImage("dbe_2016_03_24_v1.gif")
